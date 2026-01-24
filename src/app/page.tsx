71fe11548c7771665d2f2ee33038e92b10711dd0
shafiq Ur Rehman <shafiq.rehman31@gmail.com>
2026-01-24 14:21:11 +0500

    upload uplade about RAPIDLOANLINKS app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import TopHeaderOne from "@/components/TopHeaderOne";
 import Preloader from "@/helper/Preloader";
-import HeaderTwo from "@/components/HeaderTwo";
+import HeaderOne from "@/components/HeaderOne";
 import BannerOne from "@/components/BannerOne";
 import AOSWrap from "@/helper/AOSWrap";
 import OurFeatureOne from "@/components/OurFeatureOne";
@@ -53,7 +53,7 @@ export default function Home() {
       <TopHeaderOne />
 
       {/* HeaderOne */}
-      <HeaderTwo />
+      <HeaderOne />
 
       {/* BannerOne */}
       <BannerOne />
